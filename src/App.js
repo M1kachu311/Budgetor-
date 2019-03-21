dad521ff5571d16f762609f09cab868fd61d9d8f
Angot Nathan <goelnathan@gmail.com>
2019-03-21 22:19:57 +0200

    Added a temporary conditional to switch betwenn loged in and out

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
-import LeftSide from './components/LeftSide'
-import RightSide from './components/RightSide'
+import LeftSide from './components/LeftSide';
+import RightSide from './components/RightSide';
+import UserArea from './components/UserArea'
 import './App.css';
 
 class App extends Component {
+  state = { logedIn: true}; 
   render() {
-    return (
-      <div className="App">
-        <LeftSide />
-        <RightSide />
-      </div>
-    );
+    if(this.state.logedIn === false) {
+      return (
+        <div className="App">
+          <LeftSide />
+          <RightSide />
+        </div>
+      );
+    } else if (this.state.logedIn === true) {
+      return (
+        <div className="App">
+          <UserArea />
+        </div>
+      ); 
+    }
   }
 }
 
